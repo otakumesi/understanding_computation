91b6d05dd69021b15a5c4d55dde4418502b4b820
otakumesi <bakednt@gmail.com>
2018-11-21 17:15:10 +0900

    create dfa

--- section_2/run.rb
+++ section_2/run.rb
@@ -1,3 +1,7 @@
 Dir[File.expand_path('../simple', __FILE__) << '/*.rb'].each do |file|
   require file
 end
+
+Dir[File.expand_path('../automaton', __FILE__) << '/*.rb'].each do |file|
+  require file
+end
