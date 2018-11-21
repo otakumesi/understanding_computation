9d814a2c622a3e4b80b6aac58c1c32a6f8ac3011
otakumesi <bakednt@gmail.com>
2018-11-21 10:39:51 +0900

    add to_ruby to methods

--- section_1/classes/less_than.rb
+++ section_1/classes/less_than.rb
@@ -24,4 +24,8 @@ class LessThan < Struct.new(:left, :right)
   def evaluate(environment)
     Boolean.new(left.evaluate(environment).value < right.evaluate(environment).value)
   end
+
+  def to_ruby
+    "-> e { (#{left.to_ruby}).call(e) < (#{right.to_ruby}).call(e) }"
+  end
 end
