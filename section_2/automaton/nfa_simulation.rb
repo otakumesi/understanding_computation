a79e1afc68bc381e91ae7bfa1cd4b0905293cc3d
otakumesi <bakednt@gmail.com>
2018-11-24 19:07:45 +0900

    add to_dfa_design to nfa_design

--- section_2/automaton/nfa_simulation.rb
+++ section_2/automaton/nfa_simulation.rb
@@ -5,4 +5,29 @@ class NFASimulation < Struct.new(:nfa_design)
     end
     design.current_states
   end
+
+  def rules_for(state)
+    nfa_design.rulebook.alphabet.map do |character|
+      FARule.new(state, character, next_states(state, character))
+    end
+  end
+
+  def discover_states_and_rules(states)
+    rules = states.flat_map { |state| rules_for(state) }
+    more_states = rules.map(&:follow).to_set
+
+    if more_states.subset?(states)
+      [states, rules]
+    else
+      discover_states_and_rules(states + more_states)
+    end
+  end
+
+  def to_dfa_design
+    start_state = nfa_design.to_nfa.current_states
+    states, rules = discover_states_and_rules(start_state)
+    accept_states = states.select { |state| nfa_design.to_nfa(state).accepting? }
+
+    DFADesign.new(start_state, accept_states, DFARulebook.new(rules))
+  end
 end
