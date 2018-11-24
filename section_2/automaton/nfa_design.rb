5f01ad6fe16e37d4f2ebeb18e99d8732382cf62e
otakumesi <bakednt@gmail.com>
2018-11-24 13:04:11 +0900

    add variable to nfa_design

--- section_2/automaton/nfa_design.rb
+++ section_2/automaton/nfa_design.rb
@@ -3,7 +3,7 @@ class NFADesign < Struct.new(:start_state, :accept_states, :rulebook)
     to_nfa.tap { |nfa| nfa.read_string(string) }.accepting?
   end
 
-  def to_nfa
-    NFA.new(Set[start_state], accept_states, rulebook)
+  def to_nfa(current_states = Set[start_state])
+    NFA.new(current_states, accept_states, rulebook)
   end
 end
